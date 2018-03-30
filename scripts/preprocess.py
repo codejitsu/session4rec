b9c01b6aa8e2678cf3c98189fdb9ecf854c3d43a
Alex <me@codejitsu.net>
2018-03-30 12:45:09 +0200

    Added pandas dependency.

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -9,8 +9,10 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
-PATH_TO_ORIGINAL_DATA = '/path/to/clicks/dat/file/'
-PATH_TO_PROCESSED_DATA = '/path/to/store/processed/data/'
+PATH_TO_ORIGINAL_DATA = '/Users/alexsverdelov/Downloads/yoochoose-data/'
+PATH_TO_PROCESSED_DATA = '/Users/alexsverdelov/Downloads/yoochoose-data/processed/data/'
+
+print('Loading data from: {}\nOutput directory: {}'.format(PATH_TO_ORIGINAL_DATA, PATH_TO_PROCESSED_DATA))
 
 data = pd.read_csv(PATH_TO_ORIGINAL_DATA + 'yoochoose-clicks.dat', sep=',', header=None, usecols=[0,1,2], dtype={0:np.int32, 1:str, 2:np.int64})
 data.columns = ['SessionId', 'TimeStr', 'ItemId']
